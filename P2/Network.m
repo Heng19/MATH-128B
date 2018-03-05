5774277fe8d9b0972da0a4f643c468c2b0bdb086
Heng19 <Heng19@users.noreply.github.com>
2018-03-04 22:16:46 -0800

    Update
    Fk

--- P2/Network.m
+++ P2/Network.m
@@ -1,3 +1,15 @@
-function weight = Network(input, n_hlayer,n_neru)
+X = [train0',train1']';
+Y = [zeros(5923,1);ones(6742,1)];
 
+syn0 = (rand(1,784)*0.001 -0.0005)';
+
+for iter = 1:2
+
+    l1 = Neuron(X,syn0);
+    
+    l1_error = Y - l1;
+    
+    l1_delta = l1_error.*(l1.*(1-l1));
+    
+    syn0 = syn0 + double(l0)'*l1_delta;
 end
--- P2/Neuron.m
+++ P2/Neuron.m
@@ -1,8 +1,4 @@
 function ot = Neuron(input,weight)
-Net = 0
-if length(input) ~= length(weight)
-    warning('Incompatable Size of Input and Weight')
-end
-Net = input.*weight;
-ot = 1/(1+ exp(-Net));
+Net = double(input)*weight;
+ot = (1/(1+ exp(-Net.)))';
 end
