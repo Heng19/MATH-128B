12eeaa21115b60980144143f42b1b467fb155aeb
Heng19 <xuheng819@gmail.com>
2018-03-11 10:13:21 -0700

    Testing

--- P2/Network.m
+++ P2/Network.m
@@ -1,24 +1,33 @@
 load('mnist_all.mat')
-X = [train0',train1']';
-Y = [zeros(5923,1);ones(6742,1)];
+Test_data = {train0',train1',train2'};%,train3',train4',train5',train6',train7',train8',train9'};
 
-syn0 = rand(784,3)*0.001 -0.0005;
+X = cell2mat(Test_data)';
 
-syn1 = (rand(3,1)-0.5);
+Y = [zeros(size(Test_data{1},2),1)+1;zeros(size(Test_data{2},2),1)+2;zeros(size(Test_data{3},2),1)+3];
 
-for iter = 1:10000
+syn0 = rand(784,3)*0.001 -0.0005;          
+
+syn1 = (rand(3,3)*0.001-0.0005);
+
+for iter = 1:
     if mod(iter,100) == 0
         fprintf("Current %d%% \n",iter/100)
     end
     l0 = X;
 
-    l1 = Neuron(X,syn0);
+    l1 = Neuron(X,syn0,false);
     
-    l2 = Neuron(l1,syn1);
+    l2 = Neuron(l1,syn1,true);
+    
+    l2_delta = l2;
+    for i = 1:size(Y,1)
+        l2_delta(i,Y(i)) = l2_delta(i,Y(i)) - 1;
+    end
+    
 
-    Error = Y -l2;
+   % Error = Y -l2;
     
-    l2_delta = Error.*(l2.*(1-l2));
+   % l2_delta = Error.*(l2.*(1-l2));
     
     l1_delta = l2_delta*syn1'.* (l1.*(1-l1));
     
@@ -27,4 +36,5 @@ for iter = 1:10000
     syn1 = syn1 + eta * l1'*l2_delta;
     
     syn0 = syn0 + double(X)'*l1_delta;
+    
 end
--- P2/Neuron.m
+++ P2/Neuron.m
@@ -1,4 +1,11 @@
-function ot = Neuron(input,weight)
-Net = double(input)*weight;
-ot = (1./(1+ exp(-Net)));
+function ot = Neuron(input,weight,softmax)
+if softmax == false
+    Net = double(input)*weight;
+    ot = (1./(1+ exp(-Net)));
+else
+    Net = double(input)*weight;
+    ot = exp(Net)./sum(exp(Net),2);
+    
+end
+
 end
